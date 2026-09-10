f82970b413cee0d553a01417170158dd334471c9
sidharth <sidharthvijayakumar7@gmail.com>
2026-09-10 22:10:50 +0530

    Updated the code to add output

---
+++ env/dev/aws_server/output.tf
@@ -0,0 +1,11 @@
+output "ec2_instance_id" {
+  value = module.ec2.id
+}
+
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
+}
+
+output "ec2_private_ip" {
+  value = module.ec2.private_ip
+}
